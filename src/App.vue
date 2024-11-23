3882f069b00e7af3728289bb232c61e6a70f629f
Zero <samuelbustos10903@gmail.com>
2024-11-23 01:05:58 -0300

    feat: added basic styles

--- src/App.vue
+++ src/App.vue
@@ -4,4 +4,18 @@
 
 <script setup></script>
 
-<style lang="scss" scoped></style>
+<style lang="scss">
+@use './assets/sass/reset';
+@use './assets/sass/config' as *;
+
+body {
+	color: $light-color;
+	background-color: $dark-color;
+}
+h1 {
+	font-size: 1.5rem;
+}
+h2 {
+	font-size: 1.25rem;
+}
+</style>
