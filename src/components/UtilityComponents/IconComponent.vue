0b8ba620b8d3b4931b182ab6e31f961e37870a9b
Zero <samuelbustos10903@gmail.com>
2024-12-01 02:33:53 -0300

    feat: added contact component

---
+++ src/components/UtilityComponents/IconComponent.vue
@@ -0,0 +1,23 @@
+<template>
+	<svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+		<slot></slot>
+	</svg>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+@use '@/assets/sass/layout';
+@use '@/assets/sass/config' as *;
+
+.icon {
+	@include layout.square(2.5rem);
+	filter: drop-shadow($dark-shadow);
+	fill: $accent-color;
+
+	transition: $transition-time fill;
+	&:hover {
+		fill: $accent-dark-color;
+	}
+}
+</style>
