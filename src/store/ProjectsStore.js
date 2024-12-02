0f6a0f4d97d4504edd71e4ef470475fffb5945b7
Zero <samuelbustos10903@gmail.com>
2024-12-02 00:12:28 -0300

    refactor: projectsData file added

--- src/store/ProjectsStore.js
+++ src/store/ProjectsStore.js
@@ -1,7 +1,10 @@
 import { defineStore } from 'pinia';
 import { ref, computed } from 'vue';
+import { projectsData } from '../data/ProjectsData';
 
 export const useProjectsStore = defineStore('useProjects', () => {
+	const projects = ref(projectsData);
+
 	const selectedTech = ref('All');
 
 	function filterProjects(tech) {
@@ -16,49 +19,6 @@ export const useProjectsStore = defineStore('useProjects', () => {
 			return project.technologies.includes(selectedTech.value);
 		});
 	});
-
-	const projects = ref([
-		{
-			id: 1,
-			title: 'README Profile',
-			image:
-				'https://images.unsplash.com/photo-1460925895917-afdab827c52f?q=80&w=1415&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-			technologies: ['Vue', 'JavaScript', 'Sass'],
-			link: 'https://github.com/Zero10903/Zero10903',
-		},
-		{
-			id: 2,
-			title: 'Somnium',
-			image:
-				'https://images.unsplash.com/photo-1460925895917-afdab827c52f?q=80&w=1415&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-			technologies: ['C#', 'Unity'],
-			link: 'https://github.com/Zero10903/somnium',
-		},
-		{
-			id: 3,
-			title: 'Somnium',
-			image:
-				'https://images.unsplash.com/photo-1460925895917-afdab827c52f?q=80&w=1415&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-			technologies: ['C#', 'Unity'],
-			link: 'https://github.com/Zero10903/somnium',
-		},
-		{
-			id: 4,
-			title: 'Somnium',
-			image:
-				'https://images.unsplash.com/photo-1460925895917-afdab827c52f?q=80&w=1415&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-			technologies: ['C#', 'Unity'],
-			link: 'https://github.com/Zero10903/somnium',
-		},
-		{
-			id: 5,
-			title: 'Somnium',
-			image:
-				'https://images.unsplash.com/photo-1460925895917-afdab827c52f?q=80&w=1415&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-			technologies: ['C#', 'Unity'],
-			link: 'https://github.com/Zero10903/somnium',
-		},
-	]);
 
 	return { projects, selectedTech, filterProjects, filteredProjects };
 });
