035ea9ce775f55b07013a19dc7ec9ee300a73481
Zero <samuelbustos10903@gmail.com>
2024-12-07 03:50:25 -0300

    feat: added scroll animation to projects section
    Implemented a smooth scroll animation for the projects section to enhance user experience.

--- src/components/Projects/ListComponent.vue
+++ src/components/Projects/ListComponent.vue
@@ -59,6 +59,14 @@ const techIcons = {
 		&:hover {
 			background-color: $accent-color;
 		}
+
+		// ? Scroll animation
+		view-timeline-name: --projectScrollReveal;
+		view-timeline-axis: block;
+		animation-timeline: --projectScrollReveal;
+		animation-range: entry 25% cover 30%;
+		animation-fill-mode: both;
+		animation-name: bottom;
 	}
 
 	&__tech {
@@ -75,6 +83,17 @@ const techIcons = {
 		display: grid;
 		grid-template-columns: repeat(2, 1fr);
 		align-items: center;
+	}
+}
+
+@keyframes bottom {
+	from {
+		opacity: 0;
+		translate: 0% 10%;
+	}
+	to {
+		opacity: 1;
+		translate: 0% 0%;
 	}
 }
 </style>
