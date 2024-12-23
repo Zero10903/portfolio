828d4a9e6a0f7717bbfbddd7f11006ef14947f69
Zero <samuelbustos10903@gmail.com>
2024-12-22 21:38:33 -0300

    feat: registered IconComponent as global

---
+++ src/components/utility/IconComponent.vue
@@ -0,0 +1,23 @@
+<template>
+	<svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+		<slot></slot>
+	</svg>
+</template>
+
+<script lang="ts" setup></script>
+
+<style lang="scss" scoped>
+@use '@/assets/sass/layout';
+@use '@/assets/sass/config' as *;
+
+.icon {
+	@include layout.square(2rem);
+	filter: drop-shadow($dark-shadow);
+	fill: $accent-color;
+
+	transition: $transition-time all;
+	&:hover {
+		fill: $dark-accent-color;
+	}
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
-
 import App from './App.vue';
 import router from './router';
+import IconComponent from '@/components/utility/IconComponent.vue';
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+
+app.component('IconComponent', IconComponent);
 
 app.mount('#app');
