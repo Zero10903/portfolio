bef45bb93ee49c5f91e2d270246c9e15b7eff7c3
Zero <samuelbustos10903@gmail.com>
2024-12-23 18:41:23 -0300

    feat: added background image

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,6 @@
 <template>
-	<section>
+	<section class="home">
+		<article class="home__background"></article>
 		<HeroComponent />
 	</section>
 </template>
@@ -8,4 +9,15 @@
 import HeroComponent from '@/components/HeroComponent.vue';
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.home {
+	&__background {
+		height: 100dvh;
+		position: fixed;
+		inset: 0;
+		z-index: -1;
+		background: url('@/assets/images/background.png') no-repeat center
+			center/cover;
+	}
+}
+</style>
