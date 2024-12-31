c15d5597cc314917647b4b0aa95016e3fa050408
Zero <samuelbustos10903@gmail.com>
2024-12-31 16:21:09 -0300

    fix: deleted some imports that isn't using

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,5 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
-import Particles from '@tsparticles/vue3';
-import { loadSlim } from '@tsparticles/slim';
 import App from './App.vue';
 import router from './router';
 import BtnComponent from './components/utility/BtnComponent.vue';
@@ -12,11 +10,6 @@ const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
-app.use(Particles, {
-	init: async (engine) => {
-		await loadSlim(engine);
-	},
-});
 
 app.component('BtnComponent', BtnComponent);
 app.component('IconComponent', IconComponent);
