5ad514e18b4c21ee61527e58df581b533ceae0e2
Zero <samuelbustos10903@gmail.com>
2024-12-30 23:56:44 -0300

    feat: added 404 Not Found view

---
+++ src/views/NotFound.vue
@@ -0,0 +1,24 @@
+<template>
+	<section class="not-found">
+		<h1 class="not-found__title">404 - Page Not Found</h1>
+		<p>The page you are looking for does not exist.</p>
+		<router-link :to="{ name: 'home' }">
+			<BtnComponent size="lg">Go to Home</BtnComponent>
+		</router-link>
+	</section>
+</template>
+
+<script lang="ts" setup></script>
+
+<style lang="scss" scoped>
+@use '@/assets/sass/layout';
+
+.not-found {
+	height: 71dvh;
+	@include layout.flexCenter(column, 1rem);
+
+	&__title {
+		font-size: 2.5rem;
+	}
+}
+</style>
